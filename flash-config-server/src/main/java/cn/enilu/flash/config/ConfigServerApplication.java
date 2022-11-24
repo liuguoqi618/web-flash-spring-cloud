700563d374c05ba595efb9d0ca96edf424ae8be5
“liuguoqi618 <673319831@qq.com>
2022-11-24 11:43:41 +0800

    server

---
+++ flash-config-server/src/main/java/cn/enilu/flash/config/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package cn.enilu.flash.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 配置中心启动类
+ *
+ * @Author enilu
+ * @Date 2021/6/28 20:25
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class,args);
+    }
+}
